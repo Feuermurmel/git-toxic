9aeecf5c227f983e743f9d62433d7dc132aae596
Michael Schwarz <michi.schwarz@gmail.com>
2016-04-19 19:41:50 +0200

    Only one concurrent tox task by default.

--- git_toxic/__init__.py
+++ git_toxic/__init__.py
@@ -41,7 +41,7 @@ async def read_settings(repository: Repository):
 			TreeState.failure: await read_label('failure', cross_mark) },
 		max_distance = int(await repository.read_config('toxic.max-distance', 5)),
 		command = await repository.read_config('toxic.command', 'tox'),
-		max_tasks = int(await repository.read_config('toxic.max-tasks', '4')))
+		max_tasks = int(await repository.read_config('toxic.max-tasks', '1')))
 
 
 async def main(clear: bool):
